b167fd8c55a627f11bed625ee5aafea6f0767b53
Jefferson de Deus Leite <jd.leitee@gmail.com>
2018-11-26 22:06:06 -0200

    Finalizando o aplicativo implementando a animação de monstrar o checkbox após clicar no icone da ferramenta.

--- app/src/main/java/com/br/relogiodecabeceira/MainActivity.java
+++ app/src/main/java/com/br/relogiodecabeceira/MainActivity.java
@@ -9,21 +9,23 @@ import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder viewHolder = new ViewHolder();
     private Handler handler = new Handler();
     private Runnable runnable;
     private Boolean runnableStopped = false;
-
-
+    private boolean mIsBatteryOn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +38,56 @@ public class MainActivity extends AppCompatActivity {
         viewHolder.TextSeconds = findViewById(R.id.text_seconds);
         viewHolder.CheckBattery = findViewById(R.id.check_battery);
         viewHolder.textBatteryLevel = findViewById(R.id.text_battery_level);
+        viewHolder.imageOption = findViewById(R.id.image_options);
+        viewHolder.imageClose = findViewById(R.id.image_close);
+        viewHolder.linearOptions = findViewById(R.id.linear_options);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        registerReceiver(batteryreceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        registerReceiver(batteryreceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        viewHolder.CheckBattery.setChecked(true);
+
+        viewHolder.linearOptions.animate().translationY(500);
+
+        setListener();
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int id = v.getId();
+
+        if (id == R.id.check_battery) {
+            toggleCheckBaterry();
+        } else if (id == R.id.image_options) {
+            viewHolder.linearOptions.setVisibility(View.VISIBLE);
+            viewHolder.linearOptions.animate().translationY(0).setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+
+
+        } else if (id == R.id.image_close) {
+            viewHolder.linearOptions.setVisibility(View.VISIBLE);
+            viewHolder.linearOptions.animate().translationY(viewHolder.linearOptions.getMeasuredHeight()).setDuration(getResources()
+                    .getInteger(android.R.integer.config_mediumAnimTime));
+
+
+        }
+
+
+    }
+
+    private void toggleCheckBaterry() {
+        if (mIsBatteryOn) {
+            mIsBatteryOn = false;
+            viewHolder.textBatteryLevel.setVisibility(View.GONE);
+        } else {
+            mIsBatteryOn = true;
+            viewHolder.textBatteryLevel.setVisibility(View.VISIBLE);
+
+        }
     }
 
     private static class ViewHolder {
@@ -50,6 +96,9 @@ public class MainActivity extends AppCompatActivity {
         TextView TextSeconds;
         CheckBox CheckBattery;
         TextView textBatteryLevel;
+        ImageView imageOption;
+        ImageView imageClose;
+        LinearLayout linearOptions;
 
 
     }
@@ -109,10 +158,19 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
             viewHolder.textBatteryLevel.setText(String.valueOf(level) + "%");
 
 
         }
     };
+
+    private void setListener() {
+        viewHolder.CheckBattery.setOnClickListener(this);
+
+        viewHolder.imageOption.setOnClickListener(this);
+        viewHolder.imageClose.setOnClickListener(this);
+
+
+    }
 }
